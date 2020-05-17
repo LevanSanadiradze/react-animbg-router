0f20c6a0c04204293acd762723952eb47fc07199
Levan Sanadiradze <levanisana8@gmail.com>
2020-05-17 14:24:58 +0400

    Updates

--- dist/index.modern.js
+++ dist/index.modern.js
@@ -1,34 +1,68 @@
 import React, { Component, Fragment } from 'react';
 import styled from '@emotion/styled';
 
-let _ = t => t,
-    _t;
-const BgObj = styled.div(_t || (_t = _`
-    position: absolute;
+function _inheritsLoose(subClass, superClass) {
+  subClass.prototype = Object.create(superClass.prototype);
+  subClass.prototype.constructor = subClass;
+  subClass.__proto__ = superClass;
+}
 
-    background-color: ${0};
-
-    top: ${0};
-    bottom: ${0};
-    left: ${0};
-    right: ${0};
-
-    z-index: ${0};
-
-    transition-property: all;
-    transition-timing-function: ease-in-out;
-    transition-duration: ${0}s;
-`), props => props.Color, props => props.Top, props => props.Bottom, props => props.Left, props => props.Right, props => props.zIndex, props => props.TransitionDuration);
-class BackgroundObject extends Component {
-  constructor(...args) {
-    super(...args);
-
-    this.prepPosVal = val => {
-      return typeof val == 'number' ? val + 'px' : val;
-    };
+function _taggedTemplateLiteralLoose(strings, raw) {
+  if (!raw) {
+    raw = strings.slice(0);
   }
 
-  render() {
+  strings.raw = raw;
+  return strings;
+}
+
+function _templateObject() {
+  var data = _taggedTemplateLiteralLoose(["\n    position: absolute;\n\n    background-color: ", ";\n\n    top: ", ";\n    bottom: ", ";\n    left: ", ";\n    right: ", ";\n\n    z-index: ", ";\n\n    transition-property: all;\n    transition-timing-function: ease-in-out;\n    transition-duration: ", "s;\n"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+var BgObj = styled.div(_templateObject(), function (props) {
+  return props.Color;
+}, function (props) {
+  return props.Top;
+}, function (props) {
+  return props.Bottom;
+}, function (props) {
+  return props.Left;
+}, function (props) {
+  return props.Right;
+}, function (props) {
+  return props.zIndex;
+}, function (props) {
+  return props.TransitionDuration;
+});
+
+var BackgroundObject = /*#__PURE__*/function (_Component) {
+  _inheritsLoose(BackgroundObject, _Component);
+
+  function BackgroundObject() {
+    var _this;
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _Component.call.apply(_Component, [this].concat(args)) || this;
+
+    _this.prepPosVal = function (val) {
+      return typeof val == 'number' ? val + 'px' : val;
+    };
+
+    return _this;
+  }
+
+  var _proto = BackgroundObject.prototype;
+
+  _proto.render = function render() {
     return /*#__PURE__*/React.createElement(BgObj, {
       Color: this.props.Color,
       Top: this.prepPosVal(this.props.Top),
@@ -38,16 +72,21 @@ class BackgroundObject extends Component {
       zIndex: this.props.zIndex || -100,
       TransitionDuration: this.props.TransitionDuration
     });
-  }
+  };
 
-}
+  return BackgroundObject;
+}(Component);
 
-class Index extends Component {
-  constructor(props) {
-    super(props);
+var Index = /*#__PURE__*/function (_Component) {
+  _inheritsLoose(Index, _Component);
 
-    this.getBackgroundObjects = objs => {
-      return objs.map((e, i) => {
+  function Index(props) {
+    var _this;
+
+    _this = _Component.call(this, props) || this;
+
+    _this.getBackgroundObjects = function (objs) {
+      return objs.map(function (e, i) {
         return /*#__PURE__*/React.createElement(BackgroundObject, {
           key: "Router_BackgrounObjects_" + i,
           Color: e.Color,
@@ -60,47 +99,52 @@ class Index extends Component {
       });
     };
 
-    this.checkPageChange = () => {
-      if (this.props.Pageid != this.state.CurrentPageId) {
-        this.changePage(this.props.Pageid);
+    _this.checkPageChange = function () {
+      if (_this.props.Pageid != _this.state.CurrentPageId) {
+        _this.changePage(_this.props.Pageid);
       }
     };
 
-    this.changePage = newid => {
-      const newpage = this.state.PagesData[newid];
-      const newBgData = this.state.BackgroundsData;
-      newpage.BackgroundData.forEach(e => {
+    _this.changePage = function (newid) {
+      var newpage = _this.state.PagesData[newid];
+      var newBgData = _this.state.BackgroundsData;
+      newpage.BackgroundData.forEach(function (e) {
         newBgData[e.Id].Position = e.targetPosition;
         newBgData[e.Id].TransitionDuration = e.Duration;
       });
-      this.setState({
+
+      _this.setState({
         BackgroundsData: newBgData,
         CurrentPage: newpage.Foreground,
         CurrentPageId: newid
       });
     };
 
-    this.state = {
+    _this.state = {
       CurrentPageId: null,
       CurrentPage: null,
       BackgroundsData: props.BackgroundObjects,
       PagesData: props.Pages
     };
+    return _this;
   }
 
-  componentDidMount() {
+  var _proto = Index.prototype;
+
+  _proto.componentDidMount = function componentDidMount() {
     this.checkPageChange();
-  }
+  };
 
-  componentDidUpdate() {
+  _proto.componentDidUpdate = function componentDidUpdate() {
     this.checkPageChange();
-  }
+  };
 
-  render() {
+  _proto.render = function render() {
     return /*#__PURE__*/React.createElement(Fragment, null, this.getBackgroundObjects(this.state.BackgroundsData), this.state.CurrentPage);
-  }
+  };
 
-}
+  return Index;
+}(Component);
 
 export default Index;
 //# sourceMappingURL=index.modern.js.map
